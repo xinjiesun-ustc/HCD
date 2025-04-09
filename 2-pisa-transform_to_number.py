a78a632058111c882fe359df6bc5442be89522e0
xinjiesun <143868522+xinjiesun-ustc@users.noreply.github.com>
2025-04-09 16:31:24 +0800

    Update 2-pisa-transform_to_number.py
    Signed-off-by: xinjiesun <143868522+xinjiesun-ustc@users.noreply.github.com>

--- 2-pisa-transform_to_number.py
+++ 2-pisa-transform_to_number.py
@@ -1,48 +1,43 @@
-####本文件的作用是：读取原始学生答题记录的csv文件，并转换成对应的数据，如：答错是0 部分对是1 全对是2。
-####并保存对应的转换后的文件到磁盘
+# This file's purpose is to read the original student answer records CSV file and convert them into corresponding data, such as: incorrect is 0, partially correct is 1, fully correct is 2.
+# The converted data will then be saved to disk.
 
-
-# 读取数据
+# Read the data
 import pandas as pd
 import numpy as np
 
-# 读取 CSV 文件
+# Read the CSV file
 file_path = "./data/pisa-science-2015.csv"
 df = pd.read_csv(file_path)
 
-# 排除指定的字段
+# Exclude specified columns
 excluded_columns = ['STU_ID']
 question_ids = [col for col in df.columns if col not in excluded_columns]
 
-# 将答案转换为数值
+# Convert answers to numerical values
 def transform_to_number(ans):
-    if isinstance(ans, str):  # 确保 ans 是字符串类型
+    if isinstance(ans, str):  # Ensure the answer is a string type
         if 'No credit' in ans:
             return 0
         if 'Partial credit' in ans:
             return 1
         if 'Full credit' in ans:
             return 2
-    return np.NaN  # 对于其他情况返回 NaN
+    return np.NaN  # Return NaN for other cases
 
-# 应用转换
+# Apply the transformation
 for q in question_ids:
-    df[q] = df[q].map(transform_to_number)  # 映射到数字
-    #     count_of_ones = (df[q] == 1).sum()  # 统计值为 1 的数量
-    #     counts[q] = count_of_ones  # 将结果存储在字典中
+    df[q] = df[q].map(transform_to_number)  # Map to numbers
+    #     count_of_ones = (df[q] == 1).sum()  # Count the number of ones
+    #     counts[q] = count_of_ones  # Store the result in the dictionary
     #
-    # # 输出结果
-    # # 计算所有问题中 1 的数量的总和
+    # # Output results
+    # # Calculate the total number of ones across all questions
     # total_count_of_ones = sum(counts.values())
     #
-    # # 输出总和
-    # print(f"所有问题中 1 的数量的总和: {total_count_of_ones}")
+    # # Output the total
+    # print(f"Total number of ones across all questions: {total_count_of_ones}")
 
-# 保存映射后的 DataFrame 到 CSV 文件
+# Save the transformed DataFrame to a CSV file
 output_file_path = "./data/pisa-science-2015-transform_to_number-3-class.csv"
 df.to_csv(output_file_path, index=False)
-print("transform_to_number转换保存成功！")
-
-
-
-
+print("transform_to_number conversion and saving successful!")
